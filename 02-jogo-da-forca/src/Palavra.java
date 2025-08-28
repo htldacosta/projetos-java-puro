271d32f15f20fd772db71c186737660ae7ca71d6
htldacosta <hitaludcosta@gmail.com>
2025-08-28 14:36:46 -0400

    metade do projeto jogo da forca tá feito

---
+++ 02-jogo-da-forca/src/Palavra.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Palavra {
+    private final List<String> palavras =
+            Arrays.asList("faca", "garfo", "prato", "pote", "tigela", "panela");
+    private String palavraSecreta;
+    private String palavraComChute;
+
+    public void gerarPalvaraSecreta() {
+        var range = this.palavras.size() - 1;
+        var index = (int) (Math.random() * range);
+
+        this.palavraSecreta = this.palavras.get(index);
+        this.palavraComChute = "_".repeat(this.palavraSecreta.length());
+    }
+
+    public void revelarLetra(String letra) {
+        for (int i = 0; i < this.palavraSecreta.length(); i++) {
+            if (this.palavraSecreta.charAt(i) == letra.charAt(0)) {
+                this.palavraComChute = this.palavraComChute.substring(0, i)
+                        + letra + this.palavraComChute.substring(i + 1);
+            }
+
+        }
+    }
+
+    public boolean palavraCompleta() {
+        return !this.palavraComChute.contains("_");
+    }
+
+    public List<String> getPalavras() {
+        return palavras;
+    }
+
+    public String getPalavraSecreta() {
+        return palavraSecreta;
+    }
+
+    public String getPalavraComChute() {
+        return palavraComChute;
+    }
+
+    public void setPalavraComChute(String palavraComChute) {
+        this.palavraComChute = palavraComChute;
+    }
+}
