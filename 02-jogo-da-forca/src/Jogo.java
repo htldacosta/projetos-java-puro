330efc61c7988b3f28b533619329b0039254ce09
htldacosta <hitaludcosta@gmail.com>
2025-08-28 15:05:48 -0400

    projeto finalizado, falta apenas comentar trechos do código

--- 02-jogo-da-forca/src/Jogo.java
+++ 02-jogo-da-forca/src/Jogo.java
@@ -1,2 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Jogo {
+    private final Palavra palavra;
+    private final Jogador jogador;
+
+    public Jogo(Palavra palavra, Jogador jogador) {
+        this.palavra = palavra;
+        this.jogador = jogador;
+    }
+
+    public void iniciarJogo() {
+        var numeroChutes = 10;
+        var chutes = new ArrayList<String>();
+
+        this.palavra.gerarPalvaraSecreta();
+
+        var scanner = new Scanner(System.in);
+        System.out.println("Olá " + this.jogador.getNome() + ", bem-vindo ao jogo da Forca!");
+        System.out.println("Você tem " + numeroChutes + "chutes para advinhar a palavra secreta");
+        System.out.println(this.palavra.getPalavraComChute() + "\n");
+
+        while(!this.palavra.palavraCompleta() && chutes.size() < numeroChutes) {
+            System.out.println("Digite seu chute: ");
+            var chute = scanner.nextLine();
+
+            if(chutes.contains(chute)) {
+                System.out.println("Você já chutou essa letra! Tente novamente!");
+                continue;
+            }
+            chutes.add(chute);
+
+            this.palavra.revelarLetra(chute);
+
+            System.out.println("Palavra: " + this.palavra.getPalavraComChute());
+
+            if (this.palavra.palavraCompleta()) {
+                System.out.println("Parabéns " + this.jogador.getNome() + "! Você venceu");
+            } else if (chutes.size() > numeroChutes) {
+                System.out.println("Poxa " + this.jogador.getNome()
+                        + " ! Você perdeu! a palavra secreta era " + this.palavra.getPalavraSecreta());
+
+            }
+        }
+    }
 }
--- 02-jogo-da-forca/src/Main.java
+++ 02-jogo-da-forca/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        var jogador = new Jogador("Hitalu");
+        var palavra = new Palavra();
 
+        var jogo = new Jogo(palavra, jogador);
+
+        jogo.iniciarJogo();
     }
-}
+}htl
