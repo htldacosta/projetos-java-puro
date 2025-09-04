4b7b88df77c1fdb0c6e33d55250538a8252c4f8e
htldacosta <hitaludcosta@gmail.com>
2025-09-04 08:49:46 -0400

    pequenas mudanças

--- 03-calculadora/src/Numero.java
+++ 03-calculadora/src/Numero.java
@@ -1,2 +1,15 @@
 public class Numero {
+    private int valor;
+
+    public Numero(int valor) {
+        this.valor = valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getNumero() {
+        return this.valor;
+    }
 }
