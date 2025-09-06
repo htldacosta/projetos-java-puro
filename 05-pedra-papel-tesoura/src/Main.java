2603927507efc182b11522e843ef60c5d20d841f
htldacosta <hitaludcosta@gmail.com>
2025-09-06 12:02:06 -0400

    projeto pedra-papel-tesoura

--- 05-pedra-papel-tesoura/src/Main.java
+++ 05-pedra-papel-tesoura/src/Main.java
@@ -13,22 +13,52 @@ public class Main {
         // U(3) C(1) = 2, Usuario Venceu
         // U(3) C(2) = 1, Computador Venceu
 
-        Scanner leitor = new Scanner(System.in);
-        Random gerador = new Random();
-
         int numeroUsuario;
         int escolhaComputador;
+        int pontosComputador = 0;
+        int pontosUsuario = 0;
 
-        numeroUsuario = leitor.nextInt();
-        escolhaComputador = gerador.nextInt(3) + 1; // Gere um numero entre 1 e 3
-        
-        if (numeroUsuario == escolhaComputador) {
-            System.out.println("Empate.");
-        } else if ((numeroUsuario - escolhaComputador) == -1 ||
-                (numeroUsuario - escolhaComputador) == 2) {
-            System.out.println("Usuario vencedor.");
-        }else {
-            System.out.println("Computador foi vencedor.");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Jokenpo. digite 1: Papel. 2: Pedra. 3: Tesoura.");
+            Scanner leitor = new Scanner(System.in);
+            Random gerador = new Random();
+
+            numeroUsuario = leitor.nextInt();
+            escolhaComputador = gerador.nextInt(3) + 1; // Gere um numero entre 1 e 3
+
+            switch (escolhaComputador) {
+                case 1:
+                    System.out.println("Computador escolheu Papel");
+                    break;
+                case 2:
+                    System.out.println("Computador escolheu Pedra");
+                    break;
+                case 3:
+                    System.out.println("Computador escolheu Tesoura");
+                    break;
+            }
+
+            if (numeroUsuario == escolhaComputador) {
+                System.out.println("Empate.");
+            } else if ((numeroUsuario - escolhaComputador) == -1 ||
+                    (numeroUsuario - escolhaComputador) == 2) {
+                System.out.println("Vc venceu a partida.");
+                pontosUsuario++;
+            } else {
+                System.out.println("O computador venceu a partida.");
+                pontosComputador++;
+            }
         }
+        if (pontosUsuario > pontosComputador) {
+            System.out.println("Usuario venceu o jogo.");
+        } else if (pontosComputador > pontosUsuario) {
+            System.out.println("O computador venceu o jogo.");
+        }
+        else {
+            System.out.println("Empate entre os jogadores.");
+        }
+
+        System.out.println(pontosComputador + " | " + pontosUsuario);
+
     }
 }
