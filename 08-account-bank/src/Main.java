3404a169a5ff2100baf458b1915d3c873cefcf14
htldacosta <hitaludcosta@gmail.com>
2026-02-16 15:43:19 -0400

    first commit

---
+++ 08-account-bank/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static double saldo = 0;
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int 
+
+    }
+}
