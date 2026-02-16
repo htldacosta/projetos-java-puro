27192c28065db661a7de05ed1914909b9b4d7388
htldacosta <hitaludcosta@gmail.com>
2026-02-16 15:53:45 -0400

    class menu was created

--- 08-account-bank/src/Main.java
+++ 08-account-bank/src/Main.java
@@ -6,9 +6,39 @@ public class Main {
 
     private static double saldo = 0;
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        int 
+        int opcao;
+
+        do {
+
+        } while ();
+
+        scanner.close();
 
     }
+
+    private static void exibirMenu() {
+        System.out.println("\n=== AGENCIA BANCÁRIA ===");
+        System.out.println("1 - Depositar");
+        System.out.println("2 - Sacar");
+        System.out.println("3 - Ver saldo");
+        System.out.println("4 - Sair");
+        System.out.println("Escolha uma opção: ");
+
+    }
+
+    private static void depositar() {
+        System.out.print("\nDigitar o valor para depósito: ");
+        double valor = scanner.nextDouble();
+
+        if(valor > 0) {
+            saldo += valor;
+            System.out.printf("Depósito de R$%.2f realizado com sucesso!", valor);
+        } else {
+            System.out.println("Valor inválido para depósito!");
+        }
+    }
+    
 }
